1af2af6b015d692248d0ab43c1bb20f463c56fdb
xiaoxiae <tomas.slama.131@gmail.com>
2019-05-12 22:37:31 +0200

    reformat convert.py

--- convert.py
+++ convert.py
@@ -1,16 +1,17 @@
 import subprocess
-import glob
+from glob import glob
 import os
 
 # go through markdown files in the current directory
-for fileName in glob.glob("*.md"):
-    # parameters for the pandoc terminal command
-    inputFileName = '"'+fileName+'"'
-    inputFilePath = '"'+fileName.split(".")[0]+'.pdf"'
-    texParameters = "-V geometry:margin=1.5cm"
+for file_name in glob("*.md"):
+    # skip README.md
+    if file_name == "README.md":
+        continue
 
     # create the pandoc command
-    command = "pandoc "+inputFileName+" -o "+inputFilePath+" "+texParameters
+    command = 'pandoc "%s" -o "%s" -V %s' % (file_name,
+                                             "Export/%s.pdf" % file_name[:-3],
+                                             "geometry:margin=1.5cm")
 
     # execute the command
     os.system(command)

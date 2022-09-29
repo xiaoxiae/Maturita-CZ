24e891ad08b0b5b118fce71ac29b274b8c61e8aa
xiaoxiae <tomas@slama.dev>
2022-09-29 18:05:21 +0200

    Slušnější convert.py skript

--- convert.py
+++ convert.py
@@ -1,21 +1,50 @@
 #!/usr/bin/env python3
 
-import subprocess
-from glob import glob
 import os
+import shutil
+from glob import glob
+from subprocess import PIPE, Popen
+
+SOURCE_DIR = "díla"
+OUTPUT_DIR = "pdf_output"
+
+# check that pandoc is installed
+if shutil.which("pandoc") is None:
+    raise Exception(f"Pandoc není nainstalovaný/spustitelný!")
+
+# move working directory to this file's
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+# ensure the output dir exists
+if not os.path.isdir(OUTPUT_DIR):
+    os.mkdir(OUTPUT_DIR)
 
 # go through markdown files in the current directory
-for file_path in glob("./díla/*.md"):
-    file_name = file_path.split("/")[-1]
+for file_path in sorted(glob(os.path.join(SOURCE_DIR, "*.md"))):
+    print(f"Převádím '{file_path}'")
+
+    file_name = os.path.basename(file_path)
+    file_name_noext = os.path.splitext(file_name)[0]
 
     # skip README.md
-    if file_name == "README.md":
+    if file_name.lower() == "README.md".lower():
         continue
 
-    # create the pandoc command
-    command = 'pandoc "%s" -o "%s" -V %s' % (file_path,
-                                             "pdf_output/%s.pdf" % file_name[:-3],
-                                             "geometry:margin=1.5cm")
-
     # execute the command
-    os.system(command)
+    process = Popen(
+        [
+            "pandoc",
+            file_path,
+            "-o",
+            f"{os.path.join(OUTPUT_DIR, file_name_noext + '.pdf')}",
+            "-V",
+            "geometry:margin=1.5cm",
+        ],
+        stdout=PIPE,
+        stderr=PIPE,
+    )
+
+    stdout, stderr = process.communicate()
+
+    if process.returncode != 0:
+        raise Exception(f"Při běhu Pandocu nastala chyba: {stderr.decode()}")
